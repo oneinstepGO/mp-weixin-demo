df9567ee09e82deb51886d64436de181c6ae4963
aaron.shaw <aaron.shaw@hstong.com>
2023-03-14 09:59:40 +0800

    init

---
+++ spring-dynamic-routing-bean/src/test/java/com/oneinstep/demo/spring/SpringDynamicRoutingBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneinstep.demo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicRoutingBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
